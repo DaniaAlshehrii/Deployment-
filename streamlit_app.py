2f685a3c2d8689ab64d9fe15dbdf29844faf8dc9
Dani <Daniaalshehri@outlook.com>
2024-08-30 15:36:13 +0300

    'a'

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,9 @@ df = user_input_features()
 st.subheader('User Input Parameters')
 st.write(df)
 
+
 try:
-    clf = joblib.load('kmeans_model.joblib')
+    clf = joblib.load('kmean_model.joblib')
     scaler = joblib.load('scaler.joblib')
 except Exception as e:
     st.error(f"Error loading models: {e}")
